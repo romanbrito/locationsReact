c751e980649300312121576bbd5b4cecb1d7a11c
roman <romanbrito1@gmail.com>
2018-04-20 15:01:10 -0500

    fit bounds in relation to data

--- src/GoogleApiComponent.js
+++ src/GoogleApiComponent.js
@@ -2,43 +2,57 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { fitBounds } from 'google-map-react/utils'
 import apiKey from './apiKey.json'
+import data from './json/locations.json'
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 30.276153,
-      lng: -97.741885
-    },
-    zoom: 11
-  };
+  // static defaultProps = {
+  //   center: {
+  //     lat: 30.276153,
+  //     lng: -97.741885
+  //   },
+  //   zoom: 11
+  // };
+
+  // "lat": 35.46395,
+  // "lng": -97.510094
+
+  // "lat": 26.194533,
+  // "lng": -98.205324
+
+
 
   render() {
 
-    const bounds = {
-      nw: {
-        lat: 50.01038826014866,
-        lng: -118.6525866875
-      },
-      se: {
-        lat: 32.698335045970396,
-        lng: -92.0217273125
-      }
-    };
-
-// Or
-
     // const bounds = {
-    //   ne: {
+    //   nw: {
     //     lat: 50.01038826014866,
     //     lng: -118.6525866875
     //   },
-    //   sw: {
-    //     lat: 32.698335045970396,
-    //     lng: -92.0217273125
+    //   se: {
+    //     lat: 26.194533,
+    //     lng: -98.205324
     //   }
     // };
+
+// Or
+
+    const maxLat = data.locations.reduce((prev, current) => (prev.coordinates.lat > current.coordinates.lat) ? prev : current)
+    const maxLng = data.locations.reduce((prev, current) => (prev.coordinates.lng > current.coordinates.lng) ? prev : current)
+    const minLat = data.locations.reduce((prev, current) => (prev.coordinates.lat < current.coordinates.lat) ? prev : current)
+    const minLng = data.locations.reduce((prev, current) => (prev.coordinates.lng < current.coordinates.lng) ? prev : current)
+
+    const bounds = {
+      ne: {
+        lat: maxLat.coordinates.lat,
+        lng: maxLng.coordinates.lng
+      },
+      sw: {
+        lat: minLat.coordinates.lat,
+        lng: minLng.coordinates.lng
+      }
+    };
 
     const size = {
       width: 640, // Map width in pixels
@@ -55,7 +69,7 @@ class SimpleMap extends Component {
           // defaultCenter={this.props.center}
           // defaultZoom={this.props.zoom}
           defaultCenter={center}
-          defaultZoom={zoom}
+          defaultZoom={zoom + 1}
         >
           <AnyReactComponent
             lat={30.276153}
