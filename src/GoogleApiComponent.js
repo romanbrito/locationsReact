60a467a4ccf00a8691bcfaeb55386b9f8a4d95cc
roman <romanbrito1@gmail.com>
2018-04-20 16:53:51 -0500

    adding location labels to map

--- src/GoogleApiComponent.js
+++ src/GoogleApiComponent.js
@@ -1,42 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
-import { fitBounds } from 'google-map-react/utils'
+import {fitBounds} from 'google-map-react/utils'
 import apiKey from './apiKey.json'
 import data from './json/locations.json'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const AnyReactComponent = ({text}) => <h1>{text}</h1>;
 
 class SimpleMap extends Component {
-  // static defaultProps = {
-  //   center: {
-  //     lat: 30.276153,
-  //     lng: -97.741885
-  //   },
-  //   zoom: 11
-  // };
-
-  // "lat": 35.46395,
-  // "lng": -97.510094
-
-  // "lat": 26.194533,
-  // "lng": -98.205324
-
-
 
   render() {
-
-    // const bounds = {
-    //   nw: {
-    //     lat: 50.01038826014866,
-    //     lng: -118.6525866875
-    //   },
-    //   se: {
-    //     lat: 26.194533,
-    //     lng: -98.205324
-    //   }
-    // };
-
-// Or
 
     const maxLat = data.locations.reduce((prev, current) => (prev.coordinates.lat > current.coordinates.lat) ? prev : current)
     const maxLng = data.locations.reduce((prev, current) => (prev.coordinates.lng > current.coordinates.lng) ? prev : current)
@@ -63,19 +36,23 @@ class SimpleMap extends Component {
 
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{height: '100vh', width: '100%'}}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key:apiKey.googleMapsApi }}
-          // defaultCenter={this.props.center}
-          // defaultZoom={this.props.zoom}
+          bootstrapURLKeys={{key: apiKey.googleMapsApi}}
           defaultCenter={center}
           defaultZoom={zoom + 1}
         >
-          <AnyReactComponent
-            lat={30.276153}
-            lng={-97.741885}
-            text={'Downtown'}
-          />
+
+          {
+            data["locations"].map(location =>
+              <AnyReactComponent
+                key={location.label}
+                lat={location.coordinates.lat}
+                lng={location.coordinates.lng}
+                text={location.label}
+              />)
+          }
+
         </GoogleMapReact>
       </div>
     );
